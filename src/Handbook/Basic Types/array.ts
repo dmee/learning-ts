720326a73f7f51acd688e7521c2866e6f14cfc41
daming <niki@90zl.com>
2020-04-23 01:27:15 +0800

    learning basic types

---
+++ src/Handbook/Basic Types/array.ts
@@ -0,0 +1,3 @@
+let list_a: number[] = [1, 2, 3];
+
+let list_b: Array<number> = [1, 2, 3];
---
+++ src/Handbook/Basic Types/string.ts
@@ -0,0 +1,7 @@
+let name: string = 'bob';
+name = 'smith';
+
+let age: number = 37;
+let sentence: string = `Hello, my name is ${name}.
+
+I'll be ${age + 1} years old next month.`;
