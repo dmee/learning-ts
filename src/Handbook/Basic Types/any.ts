273efb0a082b40467ee44139030f54da1d8abfb3
niki <wujm@lianlianpay.com>
2020-04-23 11:18:32 +0800

    finish basic types

---
+++ src/Handbook/Basic Types/any.ts
@@ -0,0 +1,14 @@
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+
+notSure = false; // okay, definitely a boolean
+
+notSure.ifItExists(); // okay, ifItExists might exist at runtime
+notSure.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
+
+let prettySure: Object = 4;
+prettySure.toFixed(); // Error: Property 'toFixed' doesn't exist on type 'Object'.
+
+let list: any[] = [1, true, 'free'];
+
+list[1] = 100;
---
+++ src/Handbook/Basic Types/assert.ts
@@ -0,0 +1,9 @@
+let someValue: any = 'this is a string';
+
+// 尖括号法
+let strLength_a: number = (<string>someValue).length;
+let strLength_b: number = (<number>someValue).length;
+
+// as法
+let strLength_c: number = (someValue as string).length;
+let strLength_d: number = (someValue as number).length;
